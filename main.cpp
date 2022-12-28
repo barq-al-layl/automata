6c536b351225608dd5d1686cc2c17b1e2649ff5d
barq-al-layl <moh74s2001@gmail.com>
2022-12-28 14:34:48 +0200

    some changes

--- main.cpp
+++ main.cpp
@@ -12,10 +12,14 @@ const int minDuration = 1, maxDuration = 20;
 // minimum and maximum number of states
 const int minStates = 6, maxStates = 20;
 
+// class for holding the State properties.
 class State {
+    // state symbol.
     string name;
+    // state duration
     int duration;
 public:
+    // constructor for setting the name & duration.
     State(string name, int duration) :
             name(std::move(name)), duration(duration) {}
 
@@ -23,14 +27,15 @@ public:
         return name;
     }
 
-    static int calculateDuration() {
-        return minDuration + (rand() % maxDuration);
-    }
-
     int getDuration() const {
         return duration;
     }
 
+    // returns a random number from [minDuration] to [maxDuration] as duration for the state.
+    static int calculateDuration() {
+        return minDuration + (rand() % maxDuration);
+    }
+    // short to print the properties in a formatted way.
     friend ostream &operator<<(ostream &out, const State &state) {
         out << state.name << "\t==>\t" << state.duration;
         return out;
@@ -40,7 +45,7 @@ public:
 int main() {
     // seed for random number generation.
     srand(time(nullptr));
-    // setting all possible states.
+    // setting all possible states with duration set to 0.
     vector<State> states = {
             State{"X", 0},
             State{"Y", 0},
@@ -52,9 +57,10 @@ int main() {
     int numberOfStates = (minStates + (rand() % maxStates)) - 2;
     unsigned int random;
 
+    // holds a state sequence of length [numberOfStates].
     vector<State> stateSequence;
 
-    // push the start state X.
+    // push the start state [X].
     stateSequence.emplace_back(
             states[0].getName(),
             State::calculateDuration()
@@ -73,16 +79,21 @@ int main() {
             State::calculateDuration()
     );
 
+    // holds the state symbol [name] to plot it in axis X later.
     vector<string> axisX;
+    // holds the state duration to plot it in axis Y.
     vector<int> axisY;
+    // print the state sequence [name & duration] along with it order in the sequence
+    // and pushes the symbols to [axisX] & duration to [axisY].
     for (int i = 0; i < stateSequence.size(); i++) {
-        cout << i << "\t-->\t\t\t" << stateSequence[i] << '\n';
+        cout << i + 1 << "\t-->\t\t\t" << stateSequence[i] << '\n';
         axisX.emplace_back(stateSequence[i].getName() + " " + to_string(i + 1));
         axisY.emplace_back(stateSequence[i].getDuration());
     }
     // sets the size of the figure and plots it.
     plt::figure_size(1000, 640);
-    plt::stem(axisX, axisY, "r--");
+    // "ro--" => r: red color, o -> o markers, --: dashed line.
+    plt::plot(axisX, axisY, "ro--");
     plt::show();
 }
 
